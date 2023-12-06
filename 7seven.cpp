2576dad7f191fb4df11f499bff2e28b4ed678aca
cy <c2857261662@163.com>
2023-12-06 17:20:45 +0800

    test

--- 7seven.cpp
+++ 7seven.cpp
@@ -1,31 +1,65 @@
+#include <iostream>
+#include <vector>
 #include <queue>
-
-#define MVNum 100
-#define MaxInt 32767
-
+#include <iomanip>
 using namespace std;
 
-typedef struct{
-    int vex[MVNum]; //顶点表
-    int arcs[MVNum][MVNum];//邻接矩阵，无向图，无权重，1 表示有边，0表示无边。
-    int Vexnum;//顶点数
-    int arcnum; //边数
-}AMGraph;
+void bfs(int start, const vector<vector<int>>& adj_list, vector<bool>& visited, vector<int>& distances) {
+    queue<int> q;
+    q.push(start);
+    visited[start] = true;
+    distances[start] = 0;
 
-int FirstAdjVex(AMGraph G,int u){
-    int j;
-    for(j=1;j<=G.vexnum;j++){
-        if(G.arcs[u][j]==1)
-        return j;
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+
+        for (int neighbor : adj_list[node]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                distances[neighbor] = distances[node] + 1;
+                q.push(neighbor);
+            }
+        }
     }
-    return 0;
 }
 
-int NextAdjVex(AMGraph G,int u,int w){
-    int i,j;
-    for(j=w+1;j<=G.vexnum;j++){
-        if(G.arcs[u][j]==1)
-        return j;
+int main() {
+    int dataset = 1;
+    while (true) {
+        int n, m;
+        cin >> n >> m;
+
+        if (n == 0 && m == 0) break;
+
+        vector<vector<int>> adj_list(n + 1);
+
+        for (int i = 0; i < m; ++i) {
+            int a, b;
+            cin >> a >> b;
+            adj_list[a].push_back(b);
+            adj_list[b].push_back(a);
+        }
+
+        cout << dataset << endl;
+        dataset++;
+
+        for (int i = 1; i <= n; ++i) {
+            vector<bool> visited(n + 1, false);
+            vector<int> distances(n + 1, -1);
+            bfs(i, adj_list, visited, distances);
+
+            int count = 0;
+            for (int j = 1; j <= n; ++j) {
+                if (distances[j] <= 6 && distances[j] != -1) {
+                    count++;
+                }
+            }
+
+            double percentage = (static_cast<double>(count) / n) * 100;
+            cout << fixed << setprecision(2) << i << ": " << percentage << "%" << endl;
+        }
     }
+
     return 0;
 }
